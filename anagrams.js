d1f6087a62e74f3b7db399a359cda4cd2aca342d
jalagor <johnrossrogla@gmail.com>
2020-02-28 10:15:58 -0700

    created anagrams function

---
+++ anagrams.js
@@ -0,0 +1,8 @@
+// Write a function that will find all the anagrams of a word from a list. 
+// Given a word (sring) and an array of words(strings), 
+// return an array of all the anagrams or an empty array if there are none.
+
+function findAnagrams( word, list){
+
+    return list
+}
