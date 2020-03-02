225a3aae825214ae3a737ac36b59505fbf03643a
jalagor <johnrossrogla@gmail.com>
2020-03-02 10:40:49 -0700

    solved

--- anagrams.js
+++ anagrams.js
@@ -2,9 +2,16 @@
 // Given a word (sring) and an array of words(strings), 
 // return an array of all the anagrams or an empty array if there are none.
 
-function findAnagrams( word, list){
-
-    return list
+function findAnagrams( word, list ){
+    const splitWord = word.split('').sort().join()
+    let tes="guy"
+    const newList = list.filter(item=>{
+       return item.length === word.length
+    })
+    const finished =  newList.filter(item=>{
+        return item.split('').sort().join() == splitWord
+    })
+    return finished
 }
-const words = ['libl', 'llib', 'billl', 'noo']
+const words = ['libl', 'llib', 'billl', 'nooo']
 console.log(findAnagrams('bill', words))
