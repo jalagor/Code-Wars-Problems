c0f312e7e25555b90634ff85afa7d93e03724242
jalagor <johnrossrogla@gmail.com>
2020-03-02 11:01:49 -0700

    added description and starting code

---
+++ uniquenumber.js
@@ -0,0 +1,10 @@
+// There is an array with some numbers. 
+// All numbers are equal except for one. Return the unique number. 
+// Think about performance. 
+
+function findUniq(arr) {
+    
+    return arr
+}
+
+console.log(findUniq([0,0,0,0,0,0,0,0,0,10,0,0]))
