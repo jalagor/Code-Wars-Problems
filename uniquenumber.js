08622d8026b329eabace8def2f228d0fff87bde4
jalagor <johnrossrogla@gmail.com>
2020-03-02 11:34:18 -0700

    checked performance

--- uniquenumber.js
+++ uniquenumber.js
@@ -1,10 +1,19 @@
 // There is an array with some numbers. 
 // All numbers are equal except for one. Return the unique number. 
 // Think about performance. 
+const { performance } = require('perf_hooks')
+
+const numbers = Array.from({ length: 3000 },(e)=>57349).concat([2]).concat(Array.from({ length: 3000 },(e)=>57349))
+let t0 = performance.now();
 
 function findUniq(arr) {
-    
-    return arr
+    const solution = arr.filter(number=>{
+        return number !== arr[0] && number !== arr[1]   
+    })
+    return solution[0]
 }
 
-console.log(findUniq([0,0,0,0,0,0,0,0,0,10,0,0]))
+console.log(findUniq(numbers))
+var t1 = performance.now();
+
+console.log("findUniq took " + (t1 - t0) + " milliseconds.");
