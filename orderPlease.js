4a7e53486a104d44b902af6773e17ab228b02286
jalagor <johnrossrogla@gmail.com>
2020-03-09 12:46:26 -0600

    solved

--- orderPlease.js
+++ orderPlease.js
@@ -5,7 +5,21 @@
 
 // If the input string is empty, return an empty string. 
 // The words in the input String will only contain valid consecutive numbers.
+string = "4of Fo1r pe6ople g3ood th5e the2"
 
 function order(words){
-
+    let sortedArray = words.split(' ').sort(function( a, b ){
+        let aWord = a.match(/[1-9]+/g)
+        let bWord = b.match((/[1-9]+/g))
+        if (aWord < bWord) {
+            return -1
+        } if (aWord>bWord){
+            return 1
+        } else { return 0}
+    }) 
+     return words === "" ? words : sortedArray.join(' ')
 }
+ 
+
+
+console.log(order(string))
