46efa449149e865c5f64bfec176bdcf314fc7181
jalagor <johnrossrogla@gmail.com>
2020-03-16 16:50:53 -0600

    added new problem

---
+++ greedIsGood.js
@@ -0,0 +1,10 @@
+// write a function that takes in an array of 5 six-sided dice and scores the roll based on the following chart: 
+// Three 1's => 1000 points
+//  Three 6's =>  600 points
+//  Three 5's =>  500 points
+//  Three 4's =>  400 points
+//  Three 3's =>  300 points
+//  Three 2's =>  200 points
+//  One   1   =>  100 points
+//  One   5   =>   50 point
+// each di should only be counted once
