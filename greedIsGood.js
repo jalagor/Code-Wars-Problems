f692bed220c031a38e20ba30901038d1080b6676
jalagor <johnrossrogla@gmail.com>
2020-03-17 18:27:05 -0600

    update

--- greedIsGood.js
+++ greedIsGood.js
@@ -7,4 +7,30 @@
 //  Three 2's =>  200 points
 //  One   1   =>  100 points
 //  One   5   =>   50 point
-// each di should only be counted once
+// each di should only be counted once 
+ 
+function score( dice ) {
+    let diValues = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0}
+    dice.map(di => diValues[di] += 1 )
+    // let diValues = dice.sort().join('')
+    let points = 0 
+
+    
+    
+    // diValues.includes('111') ? points += 1000 :
+    // diValues.includes('222') ? points += 200 :
+    // diValues.includes('333') ? points += 300 :
+    // diValues.includes('444') ? points += 400 :
+    // diValues.includes('555') ? points += 500 :
+    // diValues.includes('666') ? points += 600 :
+    // null 
+
+    // diValues.match(/\d{3}/) ? : null
+   console.log(diValues)
+    return points
+    
+    
+  }
+
+  let rolls = [6,1,1,5,2]
+  console.log(score(rolls))
