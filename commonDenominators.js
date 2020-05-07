26f2439d56f14fe28f4925ce0580433fabcb724e
jalagor <johnrossrogla@gmail.com>
2020-05-07 14:32:07 -0600

    added description and expectation

---
+++ commonDenominators.js
@@ -0,0 +1,13 @@
+// Write a function that takes an array consisting of arrays whose elements are a pair of positive integers(nominators and denmoninators), 
+// and returns a similarly formatted sting, with the corresponding pairs being of equal value to the previous pairs, 
+// using the Least Common Denominator of the initial denominators. 
+
+// let array = [ [1, 2], [1, 3], [1, 4] ]
+// convertFrac(array) returns "(6,12)(4,12)(3,12)"
+
+function convertFrac(lst){
+  
+}
+
+let array = [ [1, 2], [1, 3], [1, 4] ]
+console.log(convertFrac(array))
