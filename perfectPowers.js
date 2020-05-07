f29908c778478dc391ffbff557ffb1581856d7f2
jalagor <johnrossrogla@gmail.com>
2020-05-06 20:58:24 -0600

    initial commit

---
+++ perfectPowers.js
@@ -0,0 +1,10 @@
+// Write a function that determines whether or not a number is  perfect power. 
+// If the given number is a perfect power, return it's exponent and base. If the integer is not, return null. 
+ 
+
+var isPP = function(n){
+    
+}  
+
+ 
+console.log(isPP(81))
