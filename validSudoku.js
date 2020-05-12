2e6dc1c5df9087fe8f1b9e1d6e9005a54500cbd6
jalagor <johnrossrogla@gmail.com>
2020-05-11 20:38:14 -0600

    added rules

---
+++ validSudoku.js
@@ -0,0 +1,10 @@
+// write a function that takes a 9x9 array of arrays, and returns true if the matrix 
+// represents a correctly solved sudoku board. single digit integers are the only valid elements. 
+// rules for a solved board:  
+// each column and row must contain 1-9 exactly once
+// each of the 9 3x3 blocks must contain 1-9 exactly once 
+// 0 represents an empty square on the board, board's must not contain empty squares
+
+function validSolution(board){
+    
+  }
