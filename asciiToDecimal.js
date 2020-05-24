6a2a52479eb73059eb8d19510f534e407cf02c66
jalagor <johnrossrogla@gmail.com>
2020-05-23 23:55:28 -0600

    added function

--- asciiToDecimal.js
+++ asciiToDecimal.js
@@ -1,1 +1,6 @@
-// create a funciton that takes in an ascii code and converts it to binary
+// create a funciton that takes in an ascii code and converts it to binary 
+
+
+function asciiToBinary(ascii){
+    
+}
