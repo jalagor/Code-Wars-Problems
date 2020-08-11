10a875135eaebaeac2f455e32f1e2778f791c0a6
jalagor <johnrossrogla@gmail.com>
2020-08-10 20:30:30 -0600

    operations connected and simple multiplication solved

--- calculator.js
+++ calculator.js
@@ -8,15 +8,37 @@
  
 const Calculator = function() {
     this.evaluate = string => { 
-        let result 
+        let result = 0, 
+        array = string.split(' ') 
         
+        for(i = 0; i< array.length; i++){
+            array[i] === '*' ? 
+            result += operations('*', array[i-1], array[i+1]) : 
+            result += 0
+        }
+        // console.log( operations('*', array[0], array[2] ) ) 
         return result
-    }
+    } 
 };   
  
-const operations = {
-    '+': (num1, num2) => { return num1 += num2 }, 
-    '-': (num1, num2) => { return num1 -= num2 }, 
-    '*': (num1, num2) => { return num1 *= num2 }, 
-    '/': (num1, num2) => { return num1 /= num2 }
-}
+function operations(operand, num1, num2){
+   const math =  {
+        '+': (num1, num2) => { return num1 += num2 }, 
+        '-': (num1, num2) => { return num1 -= num2 }, 
+        '*': (num1, num2) => { return num1 *= num2 }, 
+        '/': (num1, num2) => { return num1 /= num2 }
+    }
+    return math[operand](num1, num2)
+} 
+ 
+/* 
+regex 
+    any digit /\d+/g
+    operator [operator]
+
+*/
+let calculate = new Calculator()
+// console.log(calculate.evaluate('173')) // 173
+console.log(calculate.evaluate('2 * 3')) // 5
+// console.log(calculate.evaluate('2 - 3 - 4')) // -5
+// console.log(calculate.evaluate('10 * 5 / 2')) // 25
