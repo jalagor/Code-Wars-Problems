06d598dad18039b04c584c2000051d23a2041b90
jalagor <johnrossrogla@gmail.com>
2020-08-12 16:06:21 -0600

    new file

---
+++ moveElementToEnd.js
@@ -0,0 +1,13 @@
+function moveElementToEnd(array, valueToMove){
+
+} 
+ 
+let values = [1, 2, 3, 4, 2, 2, 5, 2] 
+let values2 = [2, 1, 2, 3, 4, 2, 2, 5]
+let values3 = [2, 1, 2, 3, 4, 2, 2, 5, 2]
+let values4 = [3, 1, 2, 3, 4, 2, 2, 5, 3] 
+
+console.log(moveElementToEnd(values, 2))
+console.log(moveElementToEnd(values2, 2))
+console.log(moveElementToEnd(values3, 2))
+console.log(moveElementToEnd(values4, 2))
