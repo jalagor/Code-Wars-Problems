95d7f8cd558c44fd6fc5f4cbcd7c79de50136862
jalagor <johnrossrogla@gmail.com>
2020-08-13 23:37:43 -0600

    commit

--- moveElementToEnd.js
+++ moveElementToEnd.js
@@ -8,8 +8,11 @@
 function moveElementToEnd(array, valueToMove){   
     for(i = 0; i<array.length; i++){
         array[i] === valueToMove ? 
-
+        array.splice(i, 1) && array.push(valueToMove) : 
+        console.log('not it chief'); 
+        console.log(array[i], i)
     }
+    return array
 } 
  
 let values = [1, 2, 3, 4, 2, 2, 5, 2] 
@@ -18,6 +21,6 @@ let values3 = [2, 1, 2, 3, 4, 2, 2, 5, 2]
 let values4 = [3, 1, 2, 3, 4, 2, 2, 5, 3] 
 
 console.log(moveElementToEnd(values, 2))
-console.log(moveElementToEnd(values2, 2))
-console.log(moveElementToEnd(values3, 2))
-console.log(moveElementToEnd(values4, 2))
+// console.log(moveElementToEnd(values2, 2))
+// console.log(moveElementToEnd(values3, 2))
+// console.log(moveElementToEnd(values4, 2))
